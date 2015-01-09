c7d541e80b5c37c32870eb8bee5b5444e63637ac
David Fisher <tibbon@gmail.com>
2015-01-09 16:20:20 -0500

    Quiz questions written.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# Methods Assessment
+![General Assembly Logo](http://i.imgur.com/ke8USTq.png)
 
-Fork and clone this repo, edit `methods.rb` to contain your answers, then send a pull request. Do not reference any external resources, notes, or past assignments, and do not use the terminal or Pry to run any code.
+# Ruby Methods Assessment
+
+## Objectives
+
+This exercse will assess your knowledge in the following:
+
+- Writing Ruby methods & scope
+- Using flow control, assignment
+
+## Instructions
+
+- Fork and clone this repo, change into the `wdi_1_ruby_assessment_methods` directory.
+- Run `bundle install`
+- Run `rspec spec` and see that all 5 tests are currently failing.
+- edit `lib/assessment.rb` to fulfill the tests in `spec/assessment_spec.rb`. Run `rspec spec` to see if your tests are passing
+- stage changes for commit `git add lib/assessment.rb`
+- create a git commit `git commit -m "YOUR MESSAGE"`
+- Push changes to your Github repo `git push origin master`
+- On Github, initiate a Pull Request to turn in your assessment
--- methods.rb
+++ methods.rb
@@ -1,52 +1,35 @@
 #### Question 1
-
-def square(x)
-  x * x
-end
-
-y = square(2)
-
-result = x # What will this line do?
-
+# Write a method called square
+# that takes one argument and
+# returns that argument multiplied it by itself
 
 #### Question 2
-
-def get_older(age)
-  age = age + 1
-end
-
-age = 10
-get_older(age)
-
-result = age # What will this line do?
-
+# Write a method called greet
+# that takes one argument, with a default value of the string 'world'
+# and returns the string of 'hello ' and the value of the argument
+# For example:
+# greet() #=> 'hello world'
+# greet('David') #=> 'hello David'
 
 #### Question 3
-
-def how_high
-  energy * energy
-end
-
-def jump
-  height = how_high
-  height - 2
-end
-
-energy = 2
-
-result = jump # What will this line do?
-
+# Write a method call repeat_string
+# that accepts a string and an optional multiplier (defaults to 3)
+# and returns the original string repeated that many times (using the * operator).
+# Do not use an options hash or keyword arguments.
+# For example:
+# repeat_string('whoa ') #=> 'whoa whoa whoa '
+# repeat_string('wat', 2) #=> 'watwat'
 
 #### Question 4
-# Write a method that accepts a string and an optional multiplier (defaults to 3)
-# and returns the original string multiplied that many times (using the * operator).
-# Do not use an options hash or keyword arguments.
+# Write a method called blackjack?
+# that takes one or more arguments that represent cards
+# that will be FixNum of 2..10 or Symbols of :jack, :queen, :king, :ace
+# and returns if the sum of the value of the cards is 21 (a blackjack)
+# :jack, :queen and :king have a value of 10
+# :ace has a value of 1 or 11, and is whichever is more advantagous to making a blackjack? true
+# For example:
+# blackjack?(2, 10) #=> false
+# blackjack?(:ace, 10) #=> true
+# blackjack?(5, 10, 6) #=> true
+# blackjack?(:ace, :ace, 2, 7) #=> true
 
-
-#### Question 5
-# Write code using this method to create the string "HELLO THERE, PUNY HUMANS"
-
-def greeting(name:, salutation: 'Hi', volume: :low)
-  greeting = "#{salutation} there, #{name}"
-  volume == :high ? greeting.upcase : greeting
-end
